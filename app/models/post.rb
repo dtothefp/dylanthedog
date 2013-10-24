a01a19cbf794338e49c9e87d41cc12dc6285281e
David Fox Powell <dtothefp@gmail.com>
2013-10-23 20:47:13 -0400

    Add location map nav item

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :custom => "400X400" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "50x50>", :custom => "400X400" }, :default_url => "/images/:style/missing.png"
   
  # gps_jpg = EXIFR::JPEG.new(:image).gps? 
 
